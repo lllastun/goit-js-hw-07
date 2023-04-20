4705a039d6af6901ec54edaaf7e750190e4044e6
lllastun <lllastun@gmail.com>
2023-04-20 04:27:42 +0300

    02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,58 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 function createLiElem(image) {
-  const {
-    preview: smallImg,
-    original: bigImg,
-    description: descriptionImg,
-  } = image;
+  const { preview: smallImg, original: bigImg, description: descriptionImg } = image;
 
-  const li = document.createElement('li'); 
-  const  a = document.createElement('a'); 
-  const img= document.createElement('img');
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  const img = document.createElement("img");
 
   li.classList.add("gallery__item");
   a.classList.add("gallery__link");
   img.classList.add("gallery__image");
 
-  a.setAttribute('href', bigImg);
-  img.setAttribute('src', smallImg, 'data-source', bigImg, 'alt', descriptionImg);
-  img.setAttribute('data-source', bigImg);
-  img.setAttribute('alt', descriptionImg);
-  
+  a.setAttribute("href", bigImg);
+  img.setAttribute("src", smallImg);
+  img.setAttribute("data-source", bigImg);
+  img.setAttribute("alt", descriptionImg);
+
   a.append(img);
   li.append(a);
-  // console.log(li);
-  // console.log(smallImg, bigImg, descriptionImg);
   return li;
 }
 
-// createLiElem(galleryItems[0])
-// // console.log(galleryItems);
-const ulElement = document.querySelector('.gallery');
-// ulElement.prepend(createLiElem(galleryItems[0]));
+const galleryArray = [];
+const ulElement = document.querySelector(".gallery");
+
 for (let i = 0; i < galleryItems.length; i++) {
-ulElement.prepend(createLiElem(galleryItems[i]));
+  galleryArray.push(createLiElem(galleryItems[i]));
 }
+ulElement.prepend(...galleryArray);
 
-ulElement.addEventListener('click', () => {
-const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600">
-`)
+ulElement.addEventListener("click", (event) => {
+  event.preventDefault();
+  const bigImg = event.target.dataset.source;
 
-instance.show()
-})
+  if (event.currentTarget !== event.target) {
+    const instance = basicLightbox.create(`
+    <img src="${bigImg}" width="800" height="600">
+    `);
+    instance.show();
 
-// ulElement.prepend(createLiElem(galleryItems[1]));
-// ulElement.prepend()
-// const createAllLiElem = (galleryItems.map(obj => createLiElem(obj))).join('')
-// console.log(JSON.stringify
+    document.addEventListener("keydown", (event) => {
+      if (event.code == "Escape") {
+        instance.close();
+      }
+    });
+  }
+});
 
-// ulElement.insertAdjacentHTML('afterbegin', createLiElem(galleryItems[0]);
-
-// console.log(createLiElem(galleryItems[0]));
-
-// galleryItems.forEach(image => createImage(image))
-
-// console.log(ulElement);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+function createLiElem(image) {
+  const { preview: smallImg, original: bigImg, description: descriptionImg } = image;
+
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  const img = document.createElement("img");
+
+  li.classList.add("gallery__item");
+  a.classList.add("gallery__link");
+  img.classList.add("gallery__image");
+
+  a.setAttribute("href", bigImg);
+  img.setAttribute("src", smallImg);
+  img.setAttribute("alt", descriptionImg);
+  img.setAttribute("title", descriptionImg);
+
+  a.append(img);
+  li.append(a);
+  return li;
+}
+
+const galleryArray = [];
+const ulElement = document.querySelector(".gallery");
+const options = {
+  captionDelay: 250,
+};
+
+for (let i = 0; i < galleryItems.length; i++) {
+  galleryArray.push(createLiElem(galleryItems[i]));
+}
+ulElement.prepend(...galleryArray);
+
+ulElement.addEventListener("click", (event) => {
+  event.preventDefault();
+    let gallery = new SimpleLightbox(".gallery a", options);
+    gallery.on();
+});
